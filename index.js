18ad15125e8c0040309162e4bb2795d002811c44
Faran56 <60573160+Faran56@users.noreply.github.com>
2020-04-19 15:24:20 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,128 @@
+var randomNumber1= (Math.floor(Math.random()*6)+1)
+var randomImage1= "images/dice"+ randomNumber1 +".png"
+var image1 = document.querySelectorAll("img")[0]
+image1.setAttribute("src", randomImage1)
+
+
+
+
+
+
+
+
+var randomNumber2= (Math.floor(Math.random()*6)+1)
+var randomImage2= "images/dice"+ randomNumber2 +".png"
+var image2 = document.querySelectorAll("img")[1]
+image2.setAttribute("src", randomImage2)
+
+
+
+if (randomImage1 > randomImage2){
+  document.querySelector("h1").innerHTML = ("🏁-Faran Wins")
+}
+else
+if (randomImage2 > randomImage1){
+  document.querySelector("h1").innerHTML = ("Zeeshan Wins-🏁")
+}
+else {
+  document.querySelector("h1").innerHTML = ("!!--Draw--!!")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //
+//
+//
+// // Generating a random number for selecting dice images
+//
+//  var randomNumber1 = Math.floor(Math.random()*6) +1;
+//  // selecting a random dice image
+//
+// var randomDiceImage = "dice" + randomNumber1 + ".png";
+//
+// // Selection source for random image as all images are in one folder
+//  var randomDiceSource = "images/"+ randomDiceImage ;
+//
+// // selecting the class of image to set attribute as img is a class with 2 items img1 and img 2
+//
+//  var image1=document.querySelectorAll("img")[0];
+//
+//  image1.setAttribute("src", randomDiceSource );
+//
+// // Player 2 dice selection
+//
+//  var randomNumber2 = Math.floor(Math.random()*6) +1;
+//
+// // Selection source for random image as all images are in one folder directly to shorten the code
+//  var randomDiceSource2 = "images/dice"+ randomNumber2 + ".png";
+// // selecting the class of image and changing value of its attribute in one code
+// document.querySelectorAll("img")[1].setAttribute("src", randomDiceSource2 );
+//
+//
+// // if player 1 wins
+//
+//   if (randomNumber1 > randomNumber2){
+//     document.querySelector("h1").innerHTML = "🎉 Player 1 Wins";
+//   }
+// else
+// // if player 2 wins
+// if (randomNumber2 > randomNumber1){
+//   document.querySelector("h1").innerHTML = "💪 Player 2 Wins";}
+//   else{
+//     document.querySelector("h1").innerHTML = "Match Draws";
+//   }
